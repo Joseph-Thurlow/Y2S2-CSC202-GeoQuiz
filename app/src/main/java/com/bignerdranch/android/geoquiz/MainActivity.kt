b8f7161b05137c9c5b730d12de03f5149c43e19f
Joseph-Thur <69287038+Zenokue@users.noreply.github.com>
2021-08-02 15:10:04 +1000

    CSC202-W2 Working

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.bignerdranch.android.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+
+        //Challenge: make the toast display at the top of the screen instead of the bottom.
+        //Use the Toast class's setGravity function.
+        trueButton.setOnClickListener { view: View ->
+            Toast.makeText(this, R.string.correct_toast,
+                Toast.LENGTH_SHORT).show()
+            Toast.setGravity(Gravity.TOP)
+        }
+        falseButton.setOnClickListener { view: View ->
+            Toast.makeText(this, R.string.incorrect_toast,
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+}
